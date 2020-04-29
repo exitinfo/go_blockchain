593abf8e6fbd280a7e9147d518fd5ba583ecb19d
exitinfo <wngus_569@naver.com>
2020-04-29 11:48:17 +0000

    pow

--- pow.go
+++ pow.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 	"bytes"
 	"crypto/sha256"
 	"time"
+	"math/big"
+	"math"
+	"log"
+	"encoding/binary"
 )
 
 type Block struct {
@@ -13,23 +16,81 @@ type Block struct {
 	Data []byte
 	PrevBlockHash []byte
 	Hash []byte
+	Nonce int
 }
 type Blockchain struct {
 	blocks []*Block
 }
 
-
-const targetBits = 24
-
-func (b *Block) SetHash() {
-	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
-	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
-	hash := sha256.Sum256(headers)
-	b.Hash = hash[:]
+type ProofOfWork struct {
+	block *Block
+	target *big.Int
 }
+const targetBits = 24
+const maxNonce = math.MaxInt64
+
+func IntToHex(num int64) []byte {
+	buff := new(bytes.Buffer)
+	err := binary.Write(buff, binary.BigEndian, num)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return buff.Bytes()
+}
+func NewProofOfWork(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-targetBits))
+
+	pow := &ProofOfWork{b, target}
+	return pow
+}
+func (pow *ProofOfWork) prepareData(nonce int) []byte{
+	data := bytes.Join(
+		[][]byte{
+			pow.block.PrevBlockHash,
+			pow.block.Data,
+			IntToHex(pow.block.Timestamp),
+			IntToHex(int64(targetBits)),
+			IntToHex(int64(nonce)),
+		},
+		[]byte{},
+	)
+	return data
+}
+
+
+func (pow *ProofOfWork) Run() (int, []byte) {
+	var hashInt big.Int
+	var hash [32]byte
+	nonce := 0
+
+	fmt.Printf("Mining the block containing %s\n", pow.block.Data)
+
+	for nonce < maxNonce {
+		data := pow.prepareData(nonce)
+		hash = sha256.Sum256(data)
+		fmt.Printf("\r%x", hash)
+
+		hashInt.SetBytes(hash[:])
+		if hashInt.Cmp(pow.target) == -1 {
+			break
+		} else {
+			nonce++
+		}
+	}
+	fmt.Print("\n\n")
+
+	return nonce, hash[:]
+}
+
 func NewBlock(data string, prevBlockHash []byte) *Block {
-	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
-	block.SetHash()
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+	
+	block.Hash = hash[:]
+	block.Nonce = nonce
 
 	return block
 }

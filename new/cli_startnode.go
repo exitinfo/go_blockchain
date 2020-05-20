8701211653121c0c75f0facd3b60d4efa7635828
exitinfo <wngus_569@naver.com>
2020-05-20 17:58:41 +0000

    network

--- new/cli_startnode.go
+++ new/cli_startnode.go
@@ -6,7 +6,7 @@ import (
 )
 
 func (cli *CLI) startNode(nodeID, minerAddress string) {
-	fmt.Printf("Startin node %s\n, nodeID")
+	fmt.Printf("Startin node %s\n", nodeID)
 	if len(minerAddress) > 0 {
 		if ValidateAddress(minerAddress) {
 			fmt.Println("Mining is on. Address to receive rewards: ", minerAddress)
